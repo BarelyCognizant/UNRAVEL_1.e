092a58cc8b47615014233c009371c6370d3e44b7
txp10aka3 <Thomascox.cpx@gmail.com>
2023-05-28 21:17:04 +0100

    Added Rudimentary Bot Functionality
    Finished the Discord bot tutorial

--- DiscordBotProject/bot.py
+++ DiscordBotProject/bot.py
@@ -3,14 +3,38 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
-client.run(TOKEN)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
+@bot.command(name="test", help="test")
+@commands.has_role("bot_tester")
+async def test(ctx):
+    await ctx.send("test")
+
+
+def start_bot():
+    bot.run(TOKEN)
