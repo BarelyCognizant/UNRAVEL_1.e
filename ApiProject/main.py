34c109b5973c3fba330d5caaab18692bb6302ee7
Maxrocket <maximetreid@gmail.com>
2023-05-29 14:06:24 +0100

    Added player creation and move to API

--- ApiProject/main.py
+++ ApiProject/main.py
@@ -20,7 +20,8 @@ async def create_map_object(mapName: str):
                 "label": "",
                 "comments": ""
             }
-        }
+        },
+        "players": {}
     }
     with open("maps/" + mapName + ".json", "x") as outfile:
         json.dump(newMapData, outfile)
@@ -101,3 +102,45 @@ async def append_label(mapName: str, id: str, comments: str):
         else:
             return {"message": "Tile does not exist."}
     return {"message": "Map does not exist."}
+
+
+@app.post("/map/{mapName}/players/{name}/{id}/{color}", status_code=201)
+async def create_player(mapName: str, name: str, id: str, color: str):
+    if os.path.isfile("maps/" + mapName + ".json"):
+        with open("maps/" + mapName + ".json") as mapJsonFile:
+            mapData = json.load(mapJsonFile)
+        if id in mapData["tiles"]:
+            for tile in mapData["tiles"]:
+                if tile == id:
+                    mapData["hash"] = str(uuid.uuid4())
+                    mapData["players"][name] = {
+                        "tileId": id,
+                        "color": color
+                    }
+                    os.remove("maps/" + mapName + ".json")
+                    with open("maps/" + mapName + ".json", "x") as outfile:
+                        json.dump(mapData, outfile)
+                    return mapData
+        else:
+            return {"message": "Tile does not exist."}
+
+
+@app.put("/map/{mapName}/players/{name}/{id}", status_code=200)
+async def move_player(mapName: str, name: str, id: str):
+    if os.path.isfile("maps/" + mapName + ".json"):
+        with open("maps/" + mapName + ".json") as mapJsonFile:
+            mapData = json.load(mapJsonFile)
+        if id in mapData["tiles"]:
+            if name in mapData["players"]:
+                for player in mapData["players"]:
+                    if player == name:
+                        mapData["hash"] = str(uuid.uuid4())
+                        mapData["players"][name]["tileId"] = id
+                        os.remove("maps/" + mapName + ".json")
+                        with open("maps/" + mapName + ".json", "x") as outfile:
+                            json.dump(mapData, outfile)
+                        return mapData
+            else:
+                return {"message": "Player does not exist."}
+        else:
+            return {"message": "Tile does not exist."}
